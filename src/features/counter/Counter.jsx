e2eb66bbb1ab9616d223783a0eef41ac985b419c
NEM-ICU <itt1819025@tec.rjt.ac.lk>
2024-03-07 01:47:20 +0530

    redux store initial setup

---
+++ src/features/counter/Counter.jsx
@@ -0,0 +1,19 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./counterSlice";
+
+const Counter = () => {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <p>{count}</p>
+      <div>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;

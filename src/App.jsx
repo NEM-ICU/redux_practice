71c79bff538bdc4bb0f99e502d43b5ec0b154ac6
NEM-ICU <itt1819025@tec.rjt.ac.lk>
2024-03-12 10:59:39 +0530

    blog-temp

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
-import Counter from "./features/counter/Counter";
+import PostsList from "./features/posts/PostsList";
+import AddPostForm from "./features/posts/AddPostForm";
 
 const App = () => {
-  return <Counter />;
+  return (
+    <>
+      <PostsList />
+      <AddPostForm />
+    </>
+  );
 };
 
 export default App;
